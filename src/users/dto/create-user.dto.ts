28ab70d79394163d6ec849c7c0b4fa3a360eaef5
QuatTrinh <quattrinh25111999@gmail.com>
2025-01-05 10:11:25 +0700

    class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,13 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class CreateUserDto {
+    @IsEmail({}, { message: 'Email is not in correct format.' })
+    @IsNotEmpty({ message: 'Email is required' })
     email: string;
+
+    @IsNotEmpty({ message: 'Passwd is required' })
     password: string;
+
     name: string;
     address: string;
 }
