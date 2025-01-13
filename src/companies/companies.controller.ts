13009f7071a6b7b594a47e1a9e812c0d8b43db69
QuatTT <quattrinh.cs@gmail.com>
2025-01-13 22:08:52 +0700

    CRUD job

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestj
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
-import { ResponseMessage, User } from 'src/decorator/customize';
+import { Public, ResponseMessage, User } from 'src/decorator/customize';
 import { IUser } from 'src/users/users.interface';
 
 @Controller('companies')
@@ -15,11 +15,13 @@ export class CompaniesController {
   }
 
   @Get()
+  @Public()
   @ResponseMessage('Get all companies')
   findAll(@Query("current") currentPage: number, @Query("pageSize") limit: number, @Query() qs: string) {
     return this.companiesService.findAll(+currentPage, +limit, qs);
   }
 
+  @Public()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.companiesService.findOne(+id);
---
+++ src/jobs/dto/create-job.dto.ts
@@ -0,0 +1,53 @@
+import { Transform, Type } from "class-transformer";
+import { IsArray, IsBoolean, IsDate, IsEmail, IsNotEmpty, IsNotEmptyObject, isObject, IsObject, IsString, Validate, ValidateNested } from "class-validator";
+import mongoose from "mongoose";
+
+class Company {
+    @IsNotEmpty({ message: 'Id is required' })
+    _id: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty({ message: 'Name is required' })
+    name: string;
+}
+
+export class CreateJobDto {
+    @IsNotEmpty({ message: 'Name is required' })
+    name: string;
+
+    @IsNotEmpty({ message: 'Email is required' })
+    @IsArray({ message: 'Skills are array' })
+    @IsString({ each: true, message: 'Skill is string' })
+    skills: string;
+
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => Company)
+    company: Company;
+
+    @IsNotEmpty({ message: 'Salary is required' })
+    salary: number;
+
+    @IsNotEmpty({ message: 'Quantity is required' })
+    quantity: number;
+
+    @IsNotEmpty({ message: 'Level is required' })
+    level: string;
+
+    @IsNotEmpty({ message: 'Start date is required' })
+    @Transform(({ value }) => new Date(value))
+    @IsDate({ message: 'Start date is not valid' })
+    startDate: Date;
+
+    @IsNotEmpty({ message: 'End date is required' })
+    @Transform(({ value }) => new Date(value))
+    @IsDate({ message: 'End date is not valid' })
+    endDate: Date;
+
+    @IsNotEmpty({ message: 'isActive is required' })
+    @IsBoolean({ message: 'isActive is not valid' })
+    isActive: boolean;
+
+
+}
+
