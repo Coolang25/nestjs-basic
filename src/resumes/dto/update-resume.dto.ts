1908bc8b93c7e061f8e3b5aad793a8ecb9511304
QuatTT <quattrinh.cs@gmail.com>
2025-01-19 17:37:46 +0700

    CRUD resume

--- src/resumes/dto/update-resume.dto.ts
+++ src/resumes/dto/update-resume.dto.ts
@@ -1,4 +1,34 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateResumeDto } from './create-resume.dto';
+import { Types } from 'mongoose';
+import { IsArray, IsEmail, IsNotEmpty, ValidatePromise } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateResumeDto extends PartialType(CreateResumeDto) {}
+class UpdatedBy {
+    @IsNotEmpty()
+    _id: Types.ObjectId
+
+    @IsNotEmpty()
+    @IsEmail()
+    email: string
+}
+
+class History {
+    @IsNotEmpty()
+    status: string
+
+    @IsNotEmpty()
+    updatedAt: Date
+
+    @ValidatePromise()
+    @IsNotEmpty()
+    @Type(() => UpdatedBy)
+    updatedBy: UpdatedBy
+}
+export class UpdateResumeDto extends PartialType(CreateResumeDto) {
+    @ValidatePromise()
+    @IsNotEmpty({ message: 'History is required' })
+    @IsArray({ message: 'History is array' })
+    @Type(() => History)
+    history: History[]
+}
