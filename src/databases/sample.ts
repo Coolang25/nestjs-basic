7527a9fb712d148ed631ee469a4dab15a3a5a538
QuatTT <quattrinh.cs@gmail.com>
2025-01-19 22:55:20 +0700

    init data

---
+++ src/databases/sample.ts
@@ -0,0 +1,24 @@
+export const ADMIN_ROLE = "SUPER ADMIN"
+export const USER_ROLE = "NORMAL USER"
+
+export const INIT_PERMISSIONS = [
+    {
+        "id": "648ab415f4328bd3153ee211",
+        "name": "Get Company with paginate",
+        "apiPath": "/api/v1/companies",
+        "method": "GET",
+        "module": "COMPANIES",
+        "createdBy": {
+            "id": "647b5188a8a243e8191855b5",
+            "email": "hoidanit@gmail.com"
+        },
+        "isDeleted": false,
+        "deletedAt": null,
+        "createdAt": "2023-06-15T06:47:49.3692",
+        "updatedAt": "2023-06-15T06:54:05.1312",
+        "__v": 0,
+        "updatedBy": {
+            "_id": "647b5188a8a243e8191855b5",
+            "email": "hoidanit@gmail.com"
+        }
+    }]
