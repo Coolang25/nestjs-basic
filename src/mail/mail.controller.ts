ea38963e4a9b036b2fdfdab715c2d0bd95ec8220
QuatTT <quattrinh.cs@gmail.com>
2025-01-21 22:55:06 +0700

    Config send basic mail

---
+++ src/mail/mail.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { Public, ResponseMessage } from 'src/decorator/customize';
+import { MailerService } from '@nestjs-modules/mailer';
+
+@Controller('mail')
+export class MailController {
+  constructor(
+    private readonly mailService: MailService,
+    private mailerService: MailerService
+  ) { }
+
+  @Get()
+  @Public()
+  @ResponseMessage('Test send email')
+  async handleTestMail() {
+    await this.mailerService.sendMail({
+      to: "quattrinh@gmail.com",
+      from: '"Support Team" <support@example.com>',
+      subject: "Welcome to ...",
+      html: '<b>welcome to ...</b>'
+    })
+  }
+}
