613bde7dd7da23e7179a6fa432ac1c28b6b1451a
QuatTT <quattrinh.cs@gmail.com>
2025-01-22 22:11:17 +0700

    pass param to mail

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -2,23 +2,53 @@ import { Controller, Get } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { Public, ResponseMessage } from 'src/decorator/customize';
 import { MailerService } from '@nestjs-modules/mailer';
+import { InjectModel } from '@nestjs/mongoose';
+import { Subscriber, SubscriberDocument } from 'src/subscribers/schemas/subscriber.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { Job, JobDocument } from 'src/jobs/schemas/job.schema';
 
 @Controller('mail')
 export class MailController {
   constructor(
+    @InjectModel(Subscriber.name)
+    private subscriberModel: SoftDeleteModel<SubscriberDocument>,
+    @InjectModel(Job.name)
+    private jobModel: SoftDeleteModel<JobDocument>,
     private readonly mailService: MailService,
-    private mailerService: MailerService
+    private mailerService: MailerService,
   ) { }
 
   @Get()
   @Public()
   @ResponseMessage('Test send email')
   async handleTestMail() {
-    await this.mailerService.sendMail({
-      to: "quattrinh@gmail.com",
-      from: '"Support Team" <support@example.com>',
-      subject: "Welcome to ...",
-      template: "test"
-    })
+    const subscribers = await this.subscriberModel.find({})
+    for (const subs of subscribers) {
+      const subsSkills = subs.skills
+      const jobWithMatchingSkills = await this.jobModel.find({ skills: { $in: subsSkills } })
+
+      if (jobWithMatchingSkills?.length) {
+        const jobs = jobWithMatchingSkills.map(item => {
+          return {
+            name: item.name,
+            company: item.company,
+            salary: item.salary,
+            skills: item.skills
+          }
+        })
+
+        await this.mailerService.sendMail({
+          to: "quattrinh@gmail.com",
+          from: '"Support Team" <support@example.com>',
+          subject: "Welcome to ...",
+          template: "job",
+          context: {
+            receiver: subs.name,
+            jobs: jobs
+          }
+        })
+      }
+
+    }
   }
 }
